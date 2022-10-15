33d09786257685924e91c93b7b671b5f13c08470
weidmarkgit <jamiepaughdev@gmail.com>
2022-10-15 19:02:19 -0400

    Add plex installer

---
+++ plex-script/install-plex-debian11.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https curl wget gnupg2 -y
+
+sudo wget -O- https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex.gpg
+
+echo deb [signed-by=/usr/share/keyrings/plex.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+
+sudo apt update
+
+sudo apt install plexmediaserver -y
+
+sudo systemctl start plexmediaserver
+sudo systemctl enable plexmediaserver
+sudo systemctl status plexmediaserver
+
+# WIP: Add nftables firewall rules
+./plex-script-files/nftables.conf > /etc/nftables.conf
