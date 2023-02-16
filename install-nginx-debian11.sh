6d1e3b3fafd4c77dbb34f1424db2d2b3877ff33a
Jamie Paugh <jamiepaughdev@gmail.com>
2023-02-16 13:39:03 -0500

    Update install-nginx-debian11.sh

--- install-nginx-debian11.sh
+++ install-nginx-debian11.sh
@@ -7,12 +7,12 @@ apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 
-# Add nginx stable to apt
+# Add nginx mainline to apt
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
 
-# Set nginx stable to priority
+# Set nginx repo to priority
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
     | sudo tee /etc/apt/preferences.d/99nginx
 
