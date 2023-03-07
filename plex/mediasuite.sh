0afab7d00aaa43f0a0d69f326831a5314573e371
Jamie Paugh <jamiepaughdev@gmail.com>
2023-03-06 19:48:20 -0500

    Update mediasuite.sh

--- plex/mediasuite.sh
+++ plex/mediasuite.sh
@@ -12,7 +12,8 @@ function installArrsuite(){
     # Add Mono repo
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     echo "deb https://download.mono-project.com/repo/debian stable-buster main" |  tee /etc/apt/sources.list.d/mono-official-stable.list
-
+    apt update
+    
     # Install MediaArea repo
     wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-20_all.deb
     dpkg -i repo-mediaarea_1.0-20_all.deb 
