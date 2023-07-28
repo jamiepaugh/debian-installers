ba6ca58989b033516c2a34d0371e0d8f22c89e1e
Jamie Paugh <jamiepaughdev@gmail.com>
2023-07-28 01:29:40 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 # Install prerequisites
-apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring certbot python3-certbot-nginx
+apt install gnupg2 certbot python3-certbot-nginx
 
-# Import official nginx signing key
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-
-# Add nginx stable to apt
-echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/debian `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
-
-# Set nginx repo to priority
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+# Add NGINX's repo
+apt-key add nginx_signing.key
+echo "deb http://nginx.org/packages/ubuntu focal nginx
+deb-src http://nginx.org/packages/ubuntu focal nginx" >> /etc/apt/sources.list
 
 # Install
 apt update -y
 apt install nginx -y
+apt install certbot
+apt install python3-certbot-nginx
 systemctl enable nginx 
