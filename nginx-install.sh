92ffa1643df47ebf7821efcd1bb35b8f7fed06b7
Jamie Paugh <jamiepaughdev@gmail.com>
2023-07-28 01:32:09 -0400

    Update nginx-install.sh

--- nginx-install.sh
+++ nginx-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install prerequisites
-apt install gnupg2 certbot python3-certbot-nginx
+apt install gnupg2 certbot python3-certbot-nginx -y
 
 # Add NGINX's repo
 apt-key add nginx_signing.key
@@ -11,6 +11,6 @@ deb-src http://nginx.org/packages/ubuntu focal nginx" >> /etc/apt/sources.list
 # Install
 apt update -y
 apt install nginx -y
-apt install certbot
-apt install python3-certbot-nginx
+apt install certbot -y
+apt install python3-certbot-nginx -y
 systemctl enable nginx 
