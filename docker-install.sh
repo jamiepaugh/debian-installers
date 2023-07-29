73055d1c9f1bbf3bfea4cd41e4beff9c59e969a7
Jamie Paugh <jamiepaughdev@gmail.com>
2023-07-29 12:06:29 -0400

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install dependencies
+apt update
+apt install ca-certificates curl gnupg
+
+# Install Docker Offical GPG key
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add Docker Official repo
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update Docker Offical repo
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Profit
+docker run hello-world
